0373a7e39de76fa26ab046a4e5f47bbad9069021
hrothwell <hrothwell@css.edu>
2022-01-08 14:35:07 -0600

    initial commit, spring intializr

---
+++ src/main/java/com/hrothwell/tylenol/tylenoladdict/TylenoladdictApplication.java
@@ -0,0 +1,13 @@
+package com.hrothwell.tylenol.tylenoladdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TylenoladdictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TylenoladdictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrothwell/tylenol/tylenoladdict/TylenoladdictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrothwell.tylenol.tylenoladdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TylenoladdictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
